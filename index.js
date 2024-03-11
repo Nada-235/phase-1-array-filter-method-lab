cfd057f575060275ffcd68b007057e3a21fdb528
Nada-123 <nada.alshakiry@gmail.com>
2024-03-11 22:05:59 +0300

    Mission Accomplished

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, searchString) {
+  const lowercaseSearch = searchString.toLowerCase();
+  return drivers.filter((driver) => driver.toLowerCase() === lowercaseSearch);
+}
+
+
+function fuzzyMatch(drivers, searchString) {
+  return drivers.filter(driver => driver.startsWith(searchString));
+}
+
+
+function matchName(drivers, searchString) {
+  return drivers.filter((driver) => driver.name === searchString);
+}
+
